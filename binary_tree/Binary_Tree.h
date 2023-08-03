77a04cf2810c4f54cc4812bcc6bf37ea2a00fd63
doovvv <zhangch523@mail2.sysu.edu.cn>
2023-08-03 15:06:36 +0800

    遍历

---
+++ binary_tree/Binary_Tree.h
@@ -0,0 +1,16 @@
+struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+        };
+//
+// Created by doov on 2023/7/29.
+//
+
+#ifndef DATA_STRUCTURES_BINARY_TREE_H
+#define DATA_STRUCTURES_BINARY_TREE_H
+
+#endif //DATA_STRUCTURES_BINARY_TREE_H
